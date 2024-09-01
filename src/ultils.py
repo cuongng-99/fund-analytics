4f34aafb61110855e40da6b541adb9f92adc0f85
cuongng <cuongnv.1512@gmail.com>
2024-09-01 07:32:48 +0700

    thêm convert NaN to None

--- src/ultils.py
+++ src/ultils.py
@@ -1,6 +1,8 @@
 import re
 import pytz
 from datetime import datetime, timezone
+import pandas as pd
+import numpy as np
 
 
 def unix_to_datetime_with_timezone(unix_timestamp):
@@ -18,3 +20,27 @@ def camel_to_snake(camel_str):
     snake_case = re.sub(r"(?<!^)(?<!_)(?=[A-Z][a-z0-9])", "_", camel_str)
     snake_case = re.sub(r"([a-z0-9])([A-Z])", r"\1_\2", snake_case).lower()
     return snake_case
+
+
+def convert_numeric_nan_to_none(df) -> pd.DataFrame:
+    """
+    This function checks all columns in the input DataFrame. If a column is of any numeric type
+    and contains NaN values, it converts the column to object type and replaces NaN values with None.
+
+    Parameters:
+        df (pd.DataFrame): Input DataFrame
+
+    Returns:
+        pd.DataFrame: Modified DataFrame with NaN replaced by None in relevant columns
+    """
+    # Iterate through each column and check for numeric columns with NaN values
+    for column in df.columns:
+        # Check if the column is of numeric type and contains NaN
+        if pd.api.types.is_numeric_dtype(df[column]) and df[column].isnull().any():
+            # Convert the column to object type
+            df[column] = df[column].astype(object)
+
+    # Replace NaN with None in the entire DataFrame
+    df = df.where(pd.notna(df), None)
+
+    return df
